729a28786dc65cd09a55bd3cb55f5489d121b70f
Sreenu Gopala <sreenugopala007@outlook.com>
2018-02-16 18:57:57 +0530

    Baseline commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Jai Ganesha';
+  myMessage = 'This is my first Angular 5 App'
+}
